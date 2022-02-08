e4d50ae24b24694e790909235622927d5a82e069
Zafer Şekerci <zafersekerci@ya.ru>
2022-02-08 22:00:23 +0300

    Models for database and Customer Controller are created

--- BupaAcibademWebApi/Startup.cs
+++ BupaAcibademWebApi/Startup.cs
@@ -1,6 +1,8 @@
+using BupaAcibademWebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +33,8 @@ namespace BupaAcibademWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BupaAcibademWebApi", Version = "v1" });
             });
+            services.AddDbContext<BupaacibademdbContext>(
+                options => options.UseSqlServer("name=ConnectionStrings:BupaAcibademDB"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
